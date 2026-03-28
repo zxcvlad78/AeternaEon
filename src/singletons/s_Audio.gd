0b055d19cd2a49dc95ee50fc1c5f589c2cb23bf4
zxcvlad78 <danyaps@mail.ru>
2026-03-28 11:48:09 +0300

    fixed memory leak in s_Audio

--- src/singletons/s_Audio.gd
+++ src/singletons/s_Audio.gd
@@ -1,8 +1,11 @@
 extends Node
 
 ##sex
-func play_global(stream:AudioStream, pos:Vector3 = Vector3.ZERO, audio_player:AudioStreamPlayer3D = AudioStreamPlayer3D.new()):
+func play_global(stream:AudioStream, pos:Vector3 = Vector3.ZERO, audio_player:AudioStreamPlayer3D = AudioStreamPlayer3D.new(), free_on_finish:bool = true):
 	get_tree().root.add_child(audio_player)
 	audio_player.stream = stream
 	audio_player.global_position = pos
 	audio_player.play()
+	
+	if free_on_finish:
+		audio_player.finished.connect(audio_player.queue_free)
